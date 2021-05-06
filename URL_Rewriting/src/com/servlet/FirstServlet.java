0832ce8b363bd020bca5a9fc3f47c15befacdd5c
NirajThagunna <nirajthagunna123@gmail.com>
2021-05-06 18:31:23 +0545

    URL Rewriting

---
+++ URL_Rewriting/src/com/servlet/FirstServlet.java
@@ -0,0 +1,42 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doService(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doService(req, resp);
+	}
+	
+	protected void doService(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		
+		String name = req.getParameter("name");
+		
+		out.println("<h2>Welcome, " + name + "</h2>");
+		
+		// URL Rewriting
+		// appending the name in the query String
+		// appending the name in the url of the second servlet
+		out.println("<a href='secondServlet?name="+ name +"'>Go to the second servlet...</a>");
+		
+		out.close();
+	}
+
+}
---
+++ URL_Rewriting/src/com/servlet/SecondServlet.java
@@ -0,0 +1,41 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SecondServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doService(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doService(req, resp);
+	}
+
+	protected void doService(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+
+		// getting the url parameter 
+		// getting the value from the query string
+		String name = req.getParameter("name");
+		
+		out.println("<h2>Hello, " + name + ", welcome to the second servlet!</h2>");
+
+		out.println("<a href='/URL_Rewriting'>Go to the homepage</a>");
+		
+		out.close();
+	}
+
+}
