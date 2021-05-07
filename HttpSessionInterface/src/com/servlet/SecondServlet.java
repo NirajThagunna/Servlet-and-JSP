50c93cc01ee70049c66be0bda97553b65c76b39b
NirajThagunna <nirajthagunna123@gmail.com>
2021-05-07 10:36:42 +0545

    HttpSession Interface

---
+++ HttpSessionInterface/src/com/servlet/SecondServlet.java
@@ -0,0 +1,43 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SecondServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doService(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doService(req, resp);
+	}
+
+	protected void doService(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		
+		// Getting the HttpSession object
+		HttpSession session = req.getSession(false);
+		
+		// Getting the attribute value
+		String uname = (String)session.getAttribute("uname");
+		
+		out.println("<h2>Welcome from the second servlet "+ uname +"</h2>");
+		out.println("<a href='/HttpSessionInterface'>Go to the homepage</a>");
+
+		out.close();
+	}
+
+}
